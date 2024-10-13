e3c53bd13f7ffab63ab95f0ab741e15763dded66
James Openshaw <open0008@hz.nl>
2024-10-13 22:14:27 +0200

    auto close faq details

--- js/temp.js
+++ js/temp.js
@@ -1,2 +1,12 @@
-console.log("hello");
-console.log(document.querySelectorAll("summary"));
+// Close the FAQ question if it's not the last one that's been clicked on
+const detailsArray = document.querySelectorAll("details");
+
+for (let i = 0; i < detailsArray.length; i++) {
+    detailsArray[i].addEventListener("click", function() {
+        for (let j = 0; j < detailsArray.length; j++) {
+            if (detailsArray[i] != detailsArray[j]) {
+                detailsArray[j].removeAttribute("open");
+            }
+        }
+    });
+}
